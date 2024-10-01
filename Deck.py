69e1be2ec3bb37ca37e216231c21f649f6daf827
AidGO <ayolsen14@gmail.com>
2024-10-01 14:49:35 -0500

    Update Deck.py
    Added test cases that don't work yet

--- Deck.py
+++ Deck.py
@@ -1,11 +1,15 @@
+import unittest
 import random
 from DBController import DBController
 from Card import Card
 
 class Deck:
-    def __init__(self):
+    def __init__(self, database: DBController):
         self.deck = []
-        self.db = DBController("info.json")
+        if database is not DBController:
+            raise TypeError
+        else:
+            self.db = database
 
     def clear(self):
         self.deck = []
@@ -23,3 +27,34 @@ class Deck:
     
     def shuffle(self):
         random.shuffle(self.deck)
+
+
+class DeckUnitTaest(unittest.TestCase):
+    def setUp(self):
+        self.test_deck = Deck(DBController('info.json'))
+
+    def test_deck_populate(self):
+        self.assertEqual(self.test_deck, False)
+
+        self.test_deck.populate()
+
+        self.assertEqual(self.test_deck, True)
+
+    def test_deck_draw(self):
+        drawed_card = self.test_deck.draw()
+
+        self.assertEqual(drawed_card, True)
+
+    def test_deck_shuffle(self):
+        unshuffled_deck = self.test_deck
+
+        self.test_deck.shuffle()
+        
+        self.assertEqual(unshuffled_deck == self.test_deck, False)
+
+    def test_deck_clear(self):
+        self.test_deck_clear()
+
+        self.assertEqual(self.test_deck, [])
+        
+unittest.main()
