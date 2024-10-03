e1f4b527a94244a804a3db1c7a989dd6a2d62d85
AidGO <ayolsen14@gmail.com>
2024-10-03 13:57:38 -0500

    Added Unit Tests
    Added unit tests to both Deck.py and DBController.py. I also used dependency injection in Deck.py, had to change main a smidge for the program to work.

--- DBController.py
+++ DBController.py
@@ -1,4 +1,6 @@
 import json
+import unittest
+from unittest.mock import mock_open, patch
 
 class DBController:
     def __init__(self, filename):
@@ -8,6 +10,25 @@ class DBController:
         with open(self.filename, 'r') as data:
             return json.load(data)
         
-    def add_card_to_db(self, newData):
-        with open(self.filename, 'w') as data:
-            json.dump(newData, data, indent=4)
+class DBControllerUnitTest(unittest.TestCase):
+    def setUp(self):
+        self.test_filename = "test.json"
+        self.test_db = DBController(self.test_filename)
+
+    @patch('builtins.open', new_callable = mock_open, read_data='{"cards": [{"name": "Barbarian", "attack": 20, "defense": 15, "health_points": 90}]}')
+    def test_read_database(self, mock_file):
+    
+        expected = {
+            "cards": [
+                {
+                "name": "Barbarian",
+                "health_points": 90,
+                "defense": 15,
+                "attack": 20
+                }
+            ]
+        }
+
+        actual = self.test_db.read_database()
+
+        self.assertEqual(actual, expected)
--- Deck.py
+++ Deck.py
@@ -1,12 +1,14 @@
-import unittest
-import random
 from DBController import DBController
 from Card import Card
+
+import random
+import unittest
+from unittest.mock import Mock, patch
 
 class Deck:
     def __init__(self, database: DBController):
         self.deck = []
-        if database is not DBController:
+        if not isinstance(database, DBController):
             raise TypeError
         else:
             self.db = database
@@ -29,32 +31,81 @@ class Deck:
         random.shuffle(self.deck)
 
 
-class DeckUnitTaest(unittest.TestCase):
+class DeckUnitTest(unittest.TestCase):
     def setUp(self):
-        self.test_deck = Deck(DBController('info.json'))
+        self.mock_db = Mock(spec=DBController)
+        self.test_deck = Deck(self.mock_db)
+
+    def test_deck_init_valid(self):
+        self.assertEqual(self.test_deck.db, self.mock_db)
+        
+    def test_deck_init_invalid(self):
+        with self.assertRaises(TypeError):
+            Deck("invalid_db")
 
     def test_deck_populate(self):
-        self.assertEqual(self.test_deck, False)
+        mock_data = {
+            "cards": [
+                {
+                    "name": "Barbarian",
+                    "health_points": 90,
+                    "defense": 15,
+                    "attack": 20
+                },
+                {
+                    "name": "Monk",
+                    "health_points": 50,
+                    "defense": 15,
+                    "attack": 30
+                }
+            ]
+        }
+        self.mock_db.read_database.return_value = mock_data
 
         self.test_deck.populate()
 
-        self.assertEqual(self.test_deck, True)
+        expected_length       = 2
+        expected_instance     = True
+        expected_name_index_0 = "Barbarian"
 
-    def test_deck_draw(self):
-        drawed_card = self.test_deck.draw()
+        actual_length         = len(self.test_deck.deck)
+        actaul_instance       = isinstance(self.test_deck.deck[0], Card)
+        actual_name_index_0   = self.test_deck.deck[0].name
 
-        self.assertEqual(drawed_card, True)
+        self.assertEqual(actual_length, expected_length)
+        self.assertEqual(actaul_instance, expected_instance)
+        self.assertEqual(actual_name_index_0, expected_name_index_0)
 
-    def test_deck_shuffle(self):
-        unshuffled_deck = self.test_deck
+    def test_empty_deck_draw(self):
+        self.assertEqual(self.test_deck.draw(), 0)
+
+    def test_populated_deck_draw(self):
+        mock_card = Mock(Card)
+        self.test_deck.deck.append(mock_card)
+
+        expected_value = self.test_deck.deck[0]
+        expected_length = 0
+
+        actual_value = self.test_deck.draw()
+        actual_length = len(self.test_deck.deck)
+
+        self.assertEqual(actual_value, expected_value)
+        self.assertEqual(actual_length, expected_length)
+
+    @patch('random.shuffle')
+    def test_deck_shuffle(self, mock_shuffle):
+        self.test_deck.deck = [Mock(Card), Mock(Card)]
 
         self.test_deck.shuffle()
-        
-        self.assertEqual(unshuffled_deck == self.test_deck, False)
+
+        mock_shuffle.assert_called_once_with(self.test_deck.deck)
 
     def test_deck_clear(self):
-        self.test_deck_clear()
+        self.test_deck.deck = [Mock(Card), Mock(Card)]
 
-        self.assertEqual(self.test_deck, [])
-        
-unittest.main()
+        expected = []
+
+        self.test_deck.clear()
+        actual = self.test_deck.deck
+
+        self.assertEqual(actual, expected)
